9cb11960c6a91c029076406c6c5ad94eca796b2c
NachoNardo <nardoso@gmail.com>
2025-09-23 22:16:55 -0300

    First commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,13 +5,12 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import jdk.jpackage.internal.model.Application
 
-    fun main() {
-        embeddedServer(Netty, port = 4207) {
-            createKtorApplication()
-        }.start(wait = true)
-    wa}
+fun main() {
+    embeddedServer(Netty, port = 4207) {
+        createKtorApplication()
+    }.start(wait = true)
+}
 
 fun Application.createKtorApplication() {
     routing {
